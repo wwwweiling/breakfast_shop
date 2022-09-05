b7dd4b26909b818c2c84d5edb8cedf7ff0fdbec0
wwwweiling <108450618+wwwweiling@users.noreply.github.com>
2022-09-05 16:50:39 +0800

    0905

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+document.addEventListener("scroll",function(event){
+    if(window.scrollY>75){
+        document.getElementById("nav_wrap").classList.add("nav_wrap2");
+        document.getElementById("nav").classList.add("fixed");
+        document.getElementById("logo").classList.add("logo2");
+    }
+    else{
+        document.getElementById("nav_wrap").classList.remove("nav_wrap2");
+        document.getElementById("nav").classList.remove("fixed");
+        document.getElementById("logo").classList.remove("logo2");
+
+    }
+
+  })
