cfa92c6b7adda24a2b1f6ae3cd36dedce2166477
wwwweiling <108450618+wwwweiling@users.noreply.github.com>
2022-09-06 17:00:42 +0800

    0906

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("scroll",function(event){
-    if(window.scrollY>75){
+    if(window.scrollY>50){
         document.getElementById("nav_wrap").classList.add("nav_wrap2");
         document.getElementById("nav").classList.add("fixed");
         document.getElementById("logo").classList.add("logo2");
