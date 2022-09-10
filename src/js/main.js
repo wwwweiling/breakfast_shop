76578b9b0f5f1ab3c4b19e010a1ddb41fea2a3dc
wwwweiling <108450618+wwwweiling@users.noreply.github.com>
2022-09-11 01:34:46 +0800

    0911

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,32 @@
 document.addEventListener("scroll",function(event){
-    if(window.scrollY>50){
+    if(window.scrollY>50 && window.innerWidth >768){
         document.getElementById("nav_wrap").classList.add("nav_wrap2");
         document.getElementById("nav").classList.add("fixed");
         document.getElementById("logo").classList.add("logo2");
     }
-    else{
+    else if(window.scrollY<50 && window.innerWidth >768){
         document.getElementById("nav_wrap").classList.remove("nav_wrap2");
         document.getElementById("nav").classList.remove("fixed");
         document.getElementById("logo").classList.remove("logo2");
 
     }
+    else{
+        document.getElementById("nav_wrap").classList.remove("nav_wrap2");
 
-  })
+    }
+
+  });
+
+//   function feedIn(){
+//     document.getElementById("right").style.transform = "translateX(0%)";
+//   }
+
+//   document.addEventListener("click",feedIn()){
+
+//   }
+
+let check = document.getElementsByClassName("nav_mobile");
+check.addEventListener("click",right());
+function right() {
+    document.getElementById("right").style.transform = "translateX(0%)";
+}
