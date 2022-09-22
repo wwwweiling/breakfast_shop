98a26ea74e7d8e18adf14bcfb08b383413d00065
wwwweiling <108450618+wwwweiling@users.noreply.github.com>
2022-09-22 13:07:46 +0800

    0922上傳

---
+++ dist/js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener("scroll",function(event){
+    if(window.scrollY>50 && window.innerWidth >768){
+        document.getElementById("nav_wrap").classList.add("nav_wrap2");
+        document.getElementById("nav").classList.add("fixed");
+        document.getElementById("logo").classList.add("logo2");
+    }
+    else if(window.scrollY<50 && window.innerWidth >768){
+        document.getElementById("nav_wrap").classList.remove("nav_wrap2");
+        document.getElementById("nav").classList.remove("fixed");
+        document.getElementById("logo").classList.remove("logo2");
+
+    }
+    else{
+        document.getElementById("nav_wrap").classList.remove("nav_wrap2");
+
+    }
+
+    })
+
+    let check = document.getElementById("check");
+    let right = document.getElementById("right");
+    
+    check.addEventListener("click", function(){
+      right.classList.toggle("right2");  
+    })
+    
+    // var hvr = document.querySelectorAll(".hvr-grow");
+    // for(var i=0;i<hvr.length;i++){
+    //     hvr.addEventListener("mouseover",function(){
+    //     document.querySelector("#hover").classList.add("hover")
+    //     })
+    // hvr.addEventListener("mouseout",function(){
+    //     document.querySelector("#hover").classList.remove("hover")
+    //     })    
+    // }
+        var hvr = document.querySelector(".hvr-grow");
+        hvr.addEventListener("mouseover",function(){
+        document.querySelector("#hover").classList.add("hover")
+        })
+        hvr.addEventListener("mouseout",function(){
+        document.querySelector("#hover").classList.remove("hover")
+        })    
