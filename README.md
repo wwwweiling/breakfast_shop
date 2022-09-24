ca86ee63059c792c27915591350d3dca8c9ac18b
wwwweiling <108450618+wwwweiling@users.noreply.github.com>
2022-09-25 03:23:36 +0800

    0925-dist

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 使用:HTML+Scss+JavaScript
 使用:node.js框架
 使用動畫效果:hover.css/animate.css/wow.js
+
+node -v  11.15.0
+nvm 1.1.9
--- src/js/main.js
+++ src/js/main.js
@@ -8,35 +8,23 @@ document.addEventListener("scroll",function(event){
         document.getElementById("nav_wrap").classList.remove("nav_wrap2");
         document.getElementById("nav").classList.remove("fixed");
         document.getElementById("logo").classList.remove("logo2");
-
     }
     else{
-        document.getElementById("nav_wrap").classList.remove("nav_wrap2");
-
+        document.getElementById("nav_wrap").classList.remove("nav_wrap2")
     }
-
-    })
-
-    let check = document.getElementById("check");
-    let right = document.getElementById("right");
+})
+let check = document.getElementById("check");
+let right = document.getElementById("right");
     
     check.addEventListener("click", function(){
-      right.classList.toggle("right2");  
+        right.classList.toggle("right2");  
     })
     
-    // var hvr = document.querySelectorAll(".hvr-grow");
-    // for(var i=0;i<hvr.length;i++){
-    //     hvr.addEventListener("mouseover",function(){
-    //     document.querySelector("#hover").classList.add("hover")
-    //     })
-    // hvr.addEventListener("mouseout",function(){
-    //     document.querySelector("#hover").classList.remove("hover")
-    //     })    
-    // }
-        var hvr = document.querySelector(".hvr-grow");
-        hvr.addEventListener("mouseover",function(){
-        document.querySelector("#hover").classList.add("hover")
-        })
-        hvr.addEventListener("mouseout",function(){
-        document.querySelector("#hover").classList.remove("hover")
-        })    
+//問題 無法使所有都套用
+var hvr = document.querySelector(".hvr-grow");
+hvr.addEventListener("mouseover",function(){
+document.querySelector(".hover").classList.add("hover2")
+})
+hvr.addEventListener("mouseout",function(){
+document.querySelector(".hover").classList.remove("hover2")
+}) 
